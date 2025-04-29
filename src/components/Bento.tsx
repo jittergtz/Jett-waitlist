74ceb51039e614c60c5ac0b998a71d836f8cf4a4
jittergtz <sandro.gantze@icloud.com>
2025-04-29 18:29:16 +0200

    add bentos

--- src/components/Bento.tsx
+++ src/components/Bento.tsx
@@ -6,24 +6,35 @@ function Bento() {
   return (
     <section className='flex min-h-screen p-6 py-28 justify-center bg-radial-[at_10%_46%] from-[#ffffff] to-[#bfbebe] to-60%'>
     <div className='w-full max-w-5xl   flex flex-col items-center justify-center gap-2'>
-    <h2 className="font-display text-transparent bg-clip-text bg-gradient-to-br from-[#000000] to-[#1f1f1f] font-[600] text-[54px] md:text-[3.5rem] tracking-tight leading-[120%] mb-2 relative">
+    <h2 className="font-display text-transparent bg-clip-text bg-gradient-to-br from-[#000000] to-[#1f1f1f] font-[600] text-[54px] md:text-[3.5rem] tracking-tight leading-[120%] mb-10 relative">
             Bring your <br className="hidden sm:block" /> own style.
           </h2>
-        <div className='bg-black border border-neutral-700/20 h-96 w-full rounded-xl'></div>
+        <div className='bg-black relative overflow-hidden  h-96 w-full rounded-xl'>
+     
+        <Image
+        src={"/raylong.jpg"}
+        alt='cover'
+        fill
+        className='object cover z-0 opacity-45 pointer-events-none'/>
+        
+       
+        </div>
+      
         <div className='flex flex-col sm:flex-row w-full gap-2 items-center'>
-            <div className='h-72 w-full relative overflow-hidden rounded-xl bg-black border border-neutral-700/20'>
+            <div className='h-72 w-full relative overflow-hidden rounded-xl bg-radial-[at_95%_25%] from-[#000000] to-zinc-900 to-75% '>
             <Image
-            src={"/betaJett.jpeg"}
-            alt='beta jett icon'
-            height={1000}
-            width={1000}
-            className='w-56 absolute sm:w-72 sm:left-5 top-10 left-7 sm:top-8 '/>
-            <h1 className='absolute top-4 left-10 text-5xl tracking-tighter font-display text-transparent bg-clip-text bg-gradient-to-br from-[#747474] to-[#ffffff]'>
-                Get Access to the Beta
-                </h1>
+        src={"/raylong.jpg"}
+        alt='cover'
+        fill
+        className='object cover opacity-45 pointer-events-none'/>
+          
             </div>
-            <div className='h-72  w-full rounded-xl bg-black border border-neutral-700/20'>
-            <p>sdpjfpodsjfpdosfjdpsofjdposfjpdosjfpodsjf</p>
+            <div className='h-72 relative w-full rounded-xl bg-black border border-neutral-700/20'>
+            <Image
+        src={"/ray1.webp"}
+        alt='cover'
+        fill
+        className='object cover opacity-35 pointer-events-none'/>
             </div>
         </div>
     </div>
--- src/components/VideoPlayer.tsx
+++ src/components/VideoPlayer.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { Send } from 'lucide-react';
 import React, { useRef, useEffect, useState } from 'react';
 
 interface VideoPlayerProps {
@@ -109,24 +110,13 @@ const VideoPlayer: React.FC<VideoPlayerProps> = ({
         Your browser does not support the video tag.
       </video>
       
-      {!isPlaying && isLoaded && (
+      { !isPlaying && isLoaded && (
         <button
           className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-30 text-neutral-400"
           onClick={handlePlayClick}
           aria-label="Play video"
         >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            fill="currentColor"
-            className="w-16 h-16"
-          >
-            <path
-              fillRule="evenodd"
-              d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm14.024-.983a1.125 1.125 0 010 1.966l-5.603 3.113A1.125 1.125 0 019 15.113V8.887c0-.857.921-1.4 1.671-.983l5.603 3.113z"
-              clipRule="evenodd"
-            />
-          </svg>
+       <Send />
         </button>
       )}
       
